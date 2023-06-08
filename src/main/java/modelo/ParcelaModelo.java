beb8f962146155587d11e27ae6e5b11e0797bea2
plaiaundi <plaiaundi@HZ309808.H012108.NET>
2023-06-08 10:43:45 +0200

    getparcela string(para  codigo)

--- src/main/java/modelo/ParcelaModelo.java
+++ src/main/java/modelo/ParcelaModelo.java
@@ -39,11 +39,36 @@ public class ParcelaModelo extends Conector {
 	public Parcela getParcela(int id) {
 
 		Parcela parcela = new Parcela();
-		String select = "select * from parcelas where id="+id;
+		String select = "select * from parcelas where id=?";
+		PreparedStatement pst;
+		try {
+			System.out.println(select);
+			pst = super.conexion.prepareStatement(select);
+			pst.setInt(1, id);
+			ResultSet resultSet = pst.executeQuery();
+			resultSet.next();
+
+			parcela.setId(resultSet.getInt(1));
+			parcela.setNumero(resultSet.getString(2));
+			parcela.setM_cuadrados(resultSet.getInt(3));
+			parcela.setPrecio_dia(resultSet.getDouble(4));
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return parcela;
+
+	}
+	public Parcela getParcela(String id) {
+
+		Parcela parcela = new Parcela();
+		String select = "select * from parcelas where numreo= ?";
 		try {
 			PreparedStatement Pst = super.conexion.prepareStatement(select);
-			ResultSet resultSet = Pst.executeQuery(select);
-			//Pst.setInt(1, id);
+			Pst.setString(1, id);
+			ResultSet resultSet = Pst.executeQuery();
 			resultSet.next();
 
 			parcela.setId(resultSet.getInt(1));
